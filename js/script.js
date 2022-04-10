6856ed2f2baa86ff647b5c3aeec870b64c8d9f90
Jason <81002127+songhx-jpg@users.noreply.github.com>
2022-04-10 18:20:01 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function dld1(){
+    window.location.href = "https://www.luogu.com.cn/fe/api/problem/downloadAttachment/80p7q6iz";
+}
+function dld2(){
+    window.location.href = "https://www.luogu.com.cn/fe/api/problem/downloadAttachment/dbdrzbm0";
+}
+function dld3(){
+    window.location.href = "https://www.luogu.com.cn/api/team/downloadFile/ucai3jgy";
+}
